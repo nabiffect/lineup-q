e1a5d4029a0018d92eaa2752a567feed8e90101e
elegant651 <korea651@gmail.com>
2018-08-30 12:55:21 +0900

    update to contract

--- server/contracts/Lineup.sol
+++ server/contracts/Lineup.sol
@@ -13,13 +13,21 @@ contract Lineup is Ownable{
   }
 
   struct Bet {  	
-  	address panel;
-  	mapping (address => address) matches;
+  	address winner_man;
+    address winner_woman;
   }
 
-  mapping (address => UserInfo) public users;
-  mapping (address => address) public likes;
+  // struct Like {
+  //   address partner;    
+  //   uint numlikes;
+  // }
+
+
+  mapping (address => UserInfo) public userinfos;
+  mapping (address => uint256) public likes_forman;
+  mapping (address => uint256) public likes_forwoman;
   
+  address[] users;
   address[] panels;
   mapping (address => Bet) public bets;
   mapping (address => uint256) public balanceOfUsers;
@@ -27,11 +35,17 @@ contract Lineup is Ownable{
   uint public amountRaised;
   bool public depositClosed = false;  
 
+  address[] winners;
+  address[] winpanels;
+
+
   //events
   event FundingGoalReached(uint amountRaisedValue);
   event GameStartEvent();
+  event BettedEvent();
   event GameEndEvent();
-  event BetCompleteEvent();
+  event DeterminedFinalList(address wm, address ww);
+  event DistributionComplete();
   
 
   function() public { //falback
@@ -45,19 +59,72 @@ contract Lineup is Ownable{
     amountRaised += amount;
   }
 
-  function checkFundsReady() external {  	
+  function checkFundsReady() external {
   	if(amount >= fundingGoal){  	  
   	  depositClosed = true;
   	  FundingGoalReached(amountRaised);
   	}
   }
 
+  //TODO
+  //like action
+  //bet action
+
+  function checkFinalList() external {
+    uint num_like_man = 0;
+    address winner_man;      
+
+    uint num_like_woman = 0;
+    address winner_woman;
+
+    for(uint i=0; i<users.length; i++) {
+      address user_addr = users[i];
+      UserType type = userinfos[user_addr].type;      
+      if(type == UserType.MALE){
+        if(likes_forman[user_addr] && likes_forman[user_addr]>num_like_man){
+          num_like_man = likes_forman[user_addr];
+          winner_man = user_addr;
+        }
+      } else if(type == UserType.FEMALE){
+        if(likes_forwoman[user_addr] && likes_forwoman[user_addr]>num_like_woman){
+          num_like_woman = likes_forwoman[user_addr];
+          winner_woman = user_addr;
+        }
+      }      
+    }
+
+    winners.push(winner_man);
+    winners.push(winner_woman);
+
+    for(uint j=0; j<panels.length; j++) {
+      address panel_addr = panels[j];
+      if(bets[panel_addr] && bets[panel_addr].winner_man==winner_man && bets[panel_addr].winner_woman==winner_woman){
+        winpanels.push(panel_addr);
+      }
+    }
+
+    DeterminedFinalList(winner_man, winner_woman);
+  }
   
 
-  function distribution() {
-    for(uint i=0; i<panels.length; i++) {
-      
+  function distribution() external{
+    if(winners.length==0) throw;
+
+    for(uint j=0; j<winners.length; j++) {
+      winners[i].send(150);
     }
+
+    uint dividents = 300.div(winpanels.length);
+    for(uint i=0; i<winpanels.length; i++) {
+      winpanels[i].send(dividents);
+    }
+
+    DistributionComplete();
+  }
+
+
+  function kill() public onlyOwner {
+    selfdestruct(msg.sender);
   }
 
 }
